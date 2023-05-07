def73037cfc0b21a267daa7206d12ea46c8cbacb
Zeyad-ElNaggar <100488223+Zeyad-ElNaggar@users.noreply.github.com>
2023-05-07 05:37:59 +0300

    Cart item and shopping cart
    adding cart item and shopping implementation
    1- adding addToCart and removeFromCart() and cartDetails()
    
    2-adding attributes of both classes

--- src/main/java/Cart/CartItem.java
+++ src/main/java/Cart/CartItem.java
@@ -1,4 +1,23 @@
 package Cart;
 
+import Products.*;
 public class CartItem {
+    private int quantity;
+    private float totalPrice;
+
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int q){
+        quantity=q;
+    }
+    public float getTotalPrice(){
+        return totalPrice;
+    }
+    public void setTotalPrice(float p){  //enter the product price per unit
+        totalPrice=p*quantity;          //calculate the overall price of the items
+    }
+
 }
--- src/main/java/Cart/ShoppingCart.java
+++ src/main/java/Cart/ShoppingCart.java
@@ -1,5 +1,95 @@
 package Cart;
+import Products.*;
+import Cart.*;
 
-public class ShoppingCart {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class ShoppingCart{
+    private int quantity;
+    private float overallPrice;
+
+    private CartItem cartItem;
+    private Catalog catalog;
+
+
+
+    private ArrayList<Item> listOfCartItems = new ArrayList<>(); //to store added items by user in it
+
+
+    public ShoppingCart() {
+
+
+        cartItem = new CartItem();
+        catalog = new Catalog();
+
+
+    }
+
+    public int getQunatity(){
+        return quantity;
+    }
+    public void setQuantity(int q){
+        quantity=q;
+    }
+
+    public float getOverallPrice(){
+        return overallPrice;
+    }
+
+    public void setOverallPrice(float p){
+        overallPrice=p;
+    }
+
+    public void addItemToCart() {
+
+        HashMap<String, ArrayList<Item>> itemMap = catalog.getItems();  //to get the map of items from catalog
+        ArrayList<Item> listOfItems = new ArrayList<>();     //to store all items from map in it
+
+        for (String s : itemMap.keySet()) {   //adding items from map to array list
+            for (Item i : itemMap.get(s)) {
+                listOfItems.add(i);
+            }
+        }
+
+
+
+        System.out.println("Enter item id\n");
+        Scanner in = new Scanner(System.in);
+        int id = in.nextInt();
+        boolean check=false;
+        for(int i=0;i<listOfItems.size();i++) {  //checking for entered id in the items
+            if(id==listOfItems.get(i).getItemId()){
+                listOfCartItems.add(listOfItems.get(i));   //if valid id then add item to cart item list
+                check=true;
+            }
+        }
+        if (check==false){
+            System.out.println("Invalid item id\n");
+        }
+
+    }
+    public void removeItemFromCart(){
+        System.out.println("Enter item id\n");
+        Scanner in = new Scanner(System.in);
+        int id = in.nextInt();
+
+        boolean check=false;
+        for(int i=0; i<listOfCartItems.size();i++){
+            if(id==listOfCartItems.get(i).getItemId()){  //if entered id equals id in cart item
+                listOfCartItems.remove(i);               //then remove cart item
+                check=true;
+            }
+        }
+        if(check==false){
+            System.out.println("Invalid id or the item in not in cart\n");
+        }
+    }
+
+    public void printCartDetails(){
+        for(int i=0;i<listOfCartItems.size();i++) {
+            listOfCartItems.get(i).printItem();
+        }
+    }
 }
